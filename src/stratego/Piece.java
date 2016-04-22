edd0225d7d1706cddd72440706dbfc061065429e
shah1762 <shah1762@C-15PCD040.d211.org>
2016-04-22 12:45:21 -0500

    resources

--- src/stratego/Piece.java
+++ src/stratego/Piece.java
@@ -9,6 +9,36 @@ package stratego;
  *
  * @author shah1762
  */
-public class Piece {
+public class Piece {   
+    private int owner;
+    private int value;
+    
+    public Piece(){
+        value=0;
+        owner=-1;
+    }
+    
+    public Piece(int num, int player){//constructor sends the player's number and the value of the piece 
+        value=num;
+        owner=player;
+        
+    }        
+            
+    public int getOwner(){
+        return owner;
+    }
+            
+    public int getValue(){
+        return value;
+    }
+    
+    public void makeVoid(){
+        owner=-1;//maybe not necessary?
+        value=0;
+    }
+    
+    public String getPath(){
+        
+    }
     
 }
