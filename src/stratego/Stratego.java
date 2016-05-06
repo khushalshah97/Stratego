cb0d0a8ade2c104c1a873e1e8f5a460b8132ce87
shah1762 <shah1762@C-15PCD040.d211.org>
2016-05-06 12:45:18 -0500

    stratego

--- src/stratego/Stratego.java
+++ src/stratego/Stratego.java
@@ -9,9 +9,14 @@ package stratego;
  *
  * @author shah1762
  */
-public class Stratego {
+public class Stratego {//6 bombs, 1 10, 1 9, 2 8, 3 7, 4 6, 4 5, 4 4, 5 3, 8 2, 1 1, 1 Flag.
     Piece[][] board = new Piece[10][10];
     public Stratego(){
+        for(int i=0;i<10;i++){
+            for(int j=0;j<10;j++){
+                
+            }
+        }
         
     }
     public Piece getPiece(int i, int j){
